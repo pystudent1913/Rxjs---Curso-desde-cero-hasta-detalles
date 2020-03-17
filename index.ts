17fad94d8a9c4312431eb6e6f7a569768c08187a
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-03-16 23:04:25 -0500

    03 subscription and unsubscribe con intervals

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { Observable, Observer } from 'rxjs';
 
 /**
- * SEGUNDA CLASE
+ * TERCERA CLASE
  * 
- * Subscripcion de observables 
+ * Subscription and unsubscribe 
+ * En el caso del intervale RXJS nos ofrece un 
+ * metodo con la funcionalidad de cancelacion ya creada
  */
 
 
-// Este observer lo podemos mandar a la subscripcion 
-// Y le dira de que forma proceder con los datos que van llegando
 const observer: Observer<any> = {
   next: value => console.log('next => ', value),
   error: err => console.warn('err => ', err),
@@ -16,30 +16,43 @@ const observer: Observer<any> = {
 }
 
 
+const intervalo$ = new Observable<number>( subs => {
 
-const obs$ = new Observable<string>( subs => {
-  subs.next( 'shania' );
+  let contador = 0;
+  // Crear un contador 1,2,3,4,5
 
-  subs.next('shania 2');
+  const intervalo = setInterval( () => {
+    //cada segundo
+    contador++;
+    subs.next(contador)
+  }, 1000);
 
-  subs.next('shania 3');
-
-  subs.next('shania 4');
-
-  // Generamos un error 
-  // const a = undefined;
-  // a.nombre = 3;
-
-  subs.complete();
-
-  subs.next('shania 5');
+  return () => {
+    clearInterval(intervalo);
+    console.log('intervalo destruido')
+  }
+})
+ 
+const subs1 = intervalo$.subscribe( num => {
+  console.log('num -> ', num);
+});
+const subs2 = intervalo$.subscribe( num => {
+  console.log('num2 -> ', num);
+});
+const subs3 = intervalo$.subscribe( num => {
+  console.log('num -> ', num);
 });
 
-obs$.subscribe( observer );
 
-// obs$.subscribe( 
-//   next => console.log('next => ', next),
-//   error => console.warn('error => ', error),
-//   () => console.info('complete')
-// );
+setTimeout(() => {
+  
+  subs1.unsubscribe();
+  subs2.unsubscribe();
+  subs3.unsubscribe();
+
+}, 3000);
+
+
+
+
 
